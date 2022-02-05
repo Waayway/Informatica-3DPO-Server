5ae6e262e0ea3cba123c8e74b35371676ac8e51f
Waayway <thijs@vanwaaij.com>
2022-02-05 11:38:08 +0100

    Added basic code for data sending and it is now sending the data from other clients to the clients

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import json
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
+import uuid
+
+data = {}
+clients = []
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -8,16 +13,24 @@ class MainHandler(tornado.web.RequestHandler):
 
 class WebSocket(tornado.websocket.WebSocketHandler):
     def open(self):
-        print("WebSocket opened")
-        print(self)
+        self.id = str(uuid.uuid4())
+        clients.append(self.id)
+        data[self.id] = {"pos": {"x":0,"y":0,"z":0}}
+        print("WebSocket opened, id: "+ self.id)
         
-
     def on_message(self, message):
-        self.write_message(u"You said: " + str(message))
-        print('Websocket: '+str(message))
+        data[self.id] = json.loads(message)
+        dataToSend = without_keys(data, [self.id])
+        self.write_message(json.dumps(dataToSend))
+        
 
     def on_close(self):
         print("WebSocket closed")
+        del data[self.id]
+        clients.remove(self.id)
+
+def without_keys(d, keys):
+    return {x: d[x] for x in d if x not in keys}
 
 def make_app():
     return tornado.web.Application([
