e54d533fa9f80e0b1d2cc2742644024bc7025478
Waayway <thijs@vanwaaij.com>
2022-02-09 09:56:05 +0100

    updated some things but scratch all of it. made a plan in readme.md

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Informatica-3DPO-Server
+
+- Max Players in config
+- Map size in config 
+- Players when connected get into the lobby.
+
+
+## Protocol
+Client connects and get send `id` this id then gets stored at both the server and client level
+### Lobby
+Client gets a list of players on connecting, on disconnect of a client the client will get a update of the current players waiting in the lobby, on ready the server will receive a message which will say
+```json 
+{"id": "ready"}
+```
+Or when not ready
+```json
+{"id": "not ready"}
+```
+both have their id's in place of the "id"
+the ready data will then get sent to all the clients.
+
+### Ingame
+Client sends every 30 seconds their Velocity instead of their position.
+The server processes the velocity to then get the position. then sends both to the client to make a smoother motion for 
--- main.py
+++ main.py
@@ -8,37 +8,49 @@ data = {}
 clients = []
 ws_connections = []
 
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Game server that is using Websockets Please ignore")
+
 
 class WebSocket(tornado.websocket.WebSocketHandler):
     def open(self):
         self.id = str(uuid.uuid4())
         clients.append(self.id)
         ws_connections.append(self)
-        data[self.id] = {"pos": {"x":0,"y":0,"z":0}}
-        print("WebSocket opened, id: "+ self.id)
+        print("WebSocket opened, id: " + self.id)
+
+        data[self.id] = {"pos": {"x": 0, "y": 0, "z": 0}}
+        
+
         for ws_connection in ws_connections:
             clientCopy = clients[:]
             clientCopy.remove(clients[ws_connections.index(ws_connection)])
             ws_connection.write_message(json.dumps(clientCopy))
-        
+
     def on_message(self, message):
         data[self.id] = json.loads(message)
         dataToSend = without_keys(data, [self.id])
         if dataToSend != {}:
             self.write_message(json.dumps(dataToSend))
-        
 
     def on_close(self):
         print("WebSocket closed")
         del data[self.id]
         clients.remove(self.id)
         ws_connections.remove(self)
+        for ws_connection in ws_connections:
+            clientCopy = clients[:]
+            clientCopy.remove(clients[ws_connections.index(ws_connection)])
+            ws_connection.write_message(json.dumps(clientCopy))
+
+
+
 
 def without_keys(d, keys):
     return {x: d[x] for x in d if x not in keys}
+
 
 def make_app():
     return tornado.web.Application([
@@ -46,8 +58,8 @@ def make_app():
         (r'/ws', WebSocket)
     ])
 
+
 if __name__ == "__main__":
     app = make_app()
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
-
